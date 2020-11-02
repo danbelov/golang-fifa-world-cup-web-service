6758527bb5f49bd0cfa9141d537cbf9146643833
Daniil Belov <danbelov@gmx.ch>
2020-11-02 17:32:56 +0100

    Creating Get handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,17 +1,34 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
 // RootHandler returns an empty body status code
-func RootHandler(res http.ResponseWriter, req *http.Request) {
-
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // ListWinners returns winners from the list
-func ListWinners(res http.ResponseWriter, req *http.Request) {
-
+func ListWinners(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	year := r.URL.Query().Get("year")
+	if year == "" {
+		wnr, err := data.ListAllJSON()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(wnr)
+	} else {
+		fwn, err := data.ListAllByYear(year)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.Write(fwn)
+	}
 }
 
 // AddNewWinner adds new winner to the list
